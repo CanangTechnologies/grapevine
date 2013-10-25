d672c6cff23503ce2864d7850c2c50af11933026
Rafizan Uda Baharum <rafizan.baharum@gmail.com>
2013-10-25 15:07:17 +0000

    Fixed pom

---
+++ src/main/java/net/canang/grapevine/client/GrapevineMessages.java
@@ -0,0 +1,14 @@
+package net.canang.grapevine.client;
+
+import com.google.gwt.i18n.client.Messages;
+
+/**
+ * @author rafizan.baharum
+ * @since 10/25/13
+ */
+public interface GrapevineMessages extends Messages {
+
+    public String buttonOk();
+
+    public String buttonNo();
+}
